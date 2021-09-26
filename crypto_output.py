b2b0cb5e5358e541f65b053b3a46131d9670a13d
1asset <82859085+1asset@users.noreply.github.com>
2021-09-26 23:36:26 +0600

    Add files via upload

---
+++ crypto_output.py
@@ -0,0 +1,12 @@
+from pycoingecko import CoinGeckoAPI
+
+cryptocurrencies = CoinGeckoAPI()
+print("Input the number of cryptocurrencies you would like to see (from 1 to 50)")
+def func_crypto():
+     N = int(input())
+     if 0 < N <= 50:
+             output = cryptocurrencies.get_coins_markets(vs_currency='usd', order='market_cap_desc', per_page = N, page = 1, sparkline = False)
+             print(output)
+
+     elif N < 1 or N > 50: print("the input should be from 1 to 50")
+func_crypto()
